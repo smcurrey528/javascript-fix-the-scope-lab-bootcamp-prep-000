33c427104c50d0c3933eba1e97db72fe25f4a060
Sarah Currey <curreysarah28@gmail.com>
2018-04-11 02:01:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,10 +27,10 @@ var funkyFunction = function outsidefunction() {
   
   }
 }
-console.log(funkyFunction)
+console.log(funkyFunction()())
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction
-console.log(funkyFunction ()())
+
 
